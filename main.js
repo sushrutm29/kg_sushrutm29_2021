da4a7608ebeefd6b03b8a4b2251301152d767077
sushrutm29 <smadhavi@stevens.edu>
2020-03-15 17:21:28 -0400

    first version with expected output

---
+++ main.js
@@ -0,0 +1,18 @@
+let main = function(str_1, str_2){
+    if(typeof str_1 !== 'string' || typeof str_2 !== 'string') return false;
+    if(str_1.length !== str_2.length) return false;
+    
+    let mapped_chars = {};
+
+    for(let i=0; i<str_1.length; i++){
+        if(str_1[i] in mapped_chars){
+            if(mapped_chars[str_1[i]] !== str_2[i]) return false;
+        }else{
+            mapped_chars[str_1[i]] = str_2[i];
+        }
+    }
+    
+    return true;
+}
+
+console.log(main(process.argv[2], process.argv[3]));
